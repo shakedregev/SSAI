6d7cb57d1ed9a9b4c116cc1fb6c5ff77aa67ece0
Shaked Regev <shakedre@stanford.edu>
2019-12-20 11:37:16 -0800

    corrected call to pcgls

--- SSAI-PCGLS.m
+++ SSAI-PCGLS.m
@@ -3,14 +3,14 @@ clear all;
 % well-conditioned
 %load('image_interp.mat');
 %load('Hardesty2.mat');
-%load('Hardesty3.mat');
+load('Hardesty3.mat');
 %load('ch8-8-b2.mat');
 %load('ch8-8-b3.mat');
 %load('ch8-8-b4.mat');
 %load('ch8-8-b5.mat');
 %load('LargeRegFile.mat');
 %load('Rucci1.mat');
-load('sls.mat');
+%load('sls.mat');
 %% problem setup
 A=Problem.A;
 onnz=nnz(A);
@@ -22,12 +22,11 @@ A2=A'*A;
 nnnz=nnz(A);
 tol2=1e-2;
 nmax=length(A2);
-x_0=spalloc(nmax,1,nmax);
 %% normalize A
 C=diag(sparse(1./sqrt(diag(A2))));
 A2=C*tril(A2,-1)*C;
 A2=A2+A2'+speye(nmax);
-tol=1e-8*norm(b);
+tol=1e-8;
 if (isnan(tol))
     exit;
 end
@@ -37,8 +36,9 @@ tic
 M=entire_r_sparse_inverse(A2,nmax,lfil);
 M=(M+M')/2;
 toc;
-%% PCGLS
+%% SPCGLS
 tic;
-[x,iter,flag]=spcgls(A,b,tol,nmax,M);
+[x,iter,flag]=spcgls(A*C,b,tol*norm(b),nmax,M);
 toc;
 disp(iter)
+
